981ee3bed8d4254c6b71d07a349a05b18bf8243e
Francesco Rubino <rubino.francesco@gmail.com>
2025-07-04 13:04:40 +0200

    fix for build and added cross compilation flake for x86_64
    I'm using the `crane` flake, I only added support for x86_64 linux. I'm
    new to nix so please make a pull request for improvements

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "gff-utils build with musl for x86_64 mainly for work on remote systems";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    crane.url = "github:ipetkov/crane";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      crane,
+      flake-utils,
+      rust-overlay,
+      ...
+    }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+
+        craneLib = (crane.mkLib pkgs).overrideToolchain (
+          p:
+          p.rust-bin.stable.latest.default.override {
+            targets = [ "x86_64-unknown-linux-musl" ];
+          }
+        );
+
+        gff-utils = craneLib.buildPackage {
+          src = craneLib.cleanCargoSource ./.;
+          strictDeps = true;
+
+          CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
+          CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
+        };
+      in
+      {
+        checks = {
+          inherit gff-utils;
+        };
+
+        packages.default = gff-utils;
+      }
+    );
+}
